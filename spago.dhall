bdbdc937510cb82bb0a37a16518a753967d16979
Marshall Bowers <elliott.codes@gmail.com>
2021-07-30 19:13:05 -0400

    Return a response from the server

--- spago.dhall
+++ spago.dhall
@@ -2,7 +2,15 @@
 , license = "MIT"
 , repository = "https://github.com/maxdeviant/peregrine"
 , dependencies =
-  [ "console", "effect", "maybe", "node-http", "prelude", "psci-support" ]
+  [ "console"
+  , "effect"
+  , "maybe"
+  , "node-buffer"
+  , "node-http"
+  , "node-streams"
+  , "prelude"
+  , "psci-support"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Peregrine.purs
+++ src/Peregrine.purs
@@ -3,13 +3,22 @@ module Peregrine where
 import Prelude
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Node.Encoding (Encoding(..))
 import Node.HTTP as Http
+import Node.Stream as Stream
 
 type RequestListener
   = Http.Request -> Http.Response -> Effect Unit
 
 mkRequestListener :: RequestListener
-mkRequestListener _req _res = pure unit
+mkRequestListener _req res = do
+  _ <- Http.setStatusCode res 200
+  _ <- Http.setHeader res "Content-Type" "text/plain"
+  let
+    responseStream = res # Http.responseAsStream
+  _ <-
+    Stream.writeString responseStream UTF8 "Hello, world!" mempty
+  Stream.end responseStream mempty
 
 defaultListenOptions :: Http.ListenOptions
 defaultListenOptions =
