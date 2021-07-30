7157a431b5957aa7d72f73cf14d0e0f2bf2fb933
Marshall Bowers <elliott.codes@gmail.com>
2021-07-29 23:16:21 -0400

    Spago init

---
+++ shell.nix
@@ -0,0 +1,22 @@
+with import <nixpkgs> {};
+
+let
+  easy-ps = import (fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "bbef4245cd6810ea84e97a47c801947bfec9fadc";
+    sha256 = "00764zbwhbn61jwb5px2syzi2f9djyl8fmbd2p8wma985af54iwx";
+  }) {
+    inherit pkgs;
+  };
+in
+stdenv.mkDerivation {
+  name = "purescript-ulid";
+
+  buildInputs = [
+    easy-ps.purs
+    easy-ps.spago
+    easy-ps.pulp
+    yarn
+  ];
+}
