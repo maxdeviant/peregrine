0fafc685c1a319fdb28901f603354cf76d0a4efd
Marshall Bowers <elliott.codes@gmail.com>
2021-07-31 10:03:39 -0400

    Define HTTP request methods

---
+++ src/Peregrine/Method.purs
@@ -0,0 +1,56 @@
+module Peregrine.Method where
+
+import Prelude
+
+-- | An HTTP request method.
+data Method
+  = Get
+  | Put
+  | Post
+  | Delete
+  | Options
+  | Head
+  | Trace
+  | Connect
+  | Patch
+
+derive instance eqMethod :: Eq Method
+
+instance showMethod :: Show Method where
+  show = case _ of
+    Get -> "GET"
+    Put -> "PUT"
+    Post -> "POST"
+    Delete -> "DELETE"
+    Options -> "OPTIONS"
+    Head -> "HEAD"
+    Trace -> "TRACE"
+    Connect -> "CONNECT"
+    Patch -> "PATCH"
+
+-- | Returns whether an HTTP request with the indicated method always supports
+-- | a payload.
+-- |
+-- | The following methods always support payloads:
+-- | - `PUT`
+-- | - `POST`
+-- | - `DELETE`
+-- | - `PATCH`
+-- |
+-- | The following methods do **not** always support payloads:
+-- | - `GET`
+-- | - `HEAD`
+-- | - `CONNECT`
+-- | - `TRACE`
+-- | - `OPTIONS`
+alwaysSupportsPayload :: Method -> Boolean
+alwaysSupportsPayload = case _ of
+  Put -> true
+  Post -> true
+  Delete -> true
+  Patch -> true
+  Get -> false
+  Head -> false
+  Connect -> false
+  Trace -> false
+  Options -> false
