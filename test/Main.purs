cac8195fc5435e9efdf7e9dffb4c619df822c16f
Marshall Bowers <elliott.codes@gmail.com>
2021-07-31 21:16:13 -0400

    Add tests for `choose`

--- test/Main.purs
+++ test/Main.purs
@@ -3,6 +3,7 @@ module Test.Main where
 import Prelude
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Peregrine.PeregrineSpec (peregrineSpec)
 import Test.Peregrine.HttpSpec (httpSpec)
 import Test.Spec (describe)
 import Test.Spec.Reporter (consoleReporter)
@@ -12,4 +13,5 @@ main :: Effect Unit
 main = do
   launchAff_ $ runSpec [ consoleReporter ]
     $ describe "Peregrine" do
+        peregrineSpec
         httpSpec
---
+++ test/Peregrine/PeregrineSpec.purs
@@ -0,0 +1,44 @@
+module Peregrine.PeregrineSpec where
+
+import Prelude
+import Data.Maybe (Maybe(..), isNothing)
+import Peregrine (choose)
+import Peregrine.Http.Headers as Headers
+import Peregrine.Http.Method (Method(..))
+import Peregrine.Http.Status as Status
+import Peregrine.Request (Request)
+import Peregrine.Response (Response)
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+
+peregrineSpec :: Spec Unit
+peregrineSpec = do
+  describe "choose" do
+    let
+      req =
+        { method: Get
+        , url: "/"
+        , headers: Headers.empty
+        } ::
+          Request
+    describe "given an empty list of handlers" do
+      it "returns Nothing" do
+        result <- choose [] req
+        isNothing result `shouldEqual` true
+    describe "given a list of handlers" do
+      it "returns the first Just response" do
+        let
+          emptyResponse =
+            { status: Nothing
+            , headers: Headers.empty
+            , writeBody: Nothing
+            } ::
+              Response
+        result <-
+          choose
+            [ \_req -> pure $ Nothing
+            , \_req -> pure $ Just emptyResponse { status = Just Status.ok }
+            , \_req -> pure $ Just emptyResponse { status = Just Status.unauthorized }
+            ]
+            req
+        (result >>= _.status) `shouldEqual` Just Status.ok
