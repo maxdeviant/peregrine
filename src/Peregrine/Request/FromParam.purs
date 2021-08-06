355fc75cbbf495b28aad356607de9a0ceae56d51
Marshall Bowers <elliott.codes@gmail.com>
2021-08-05 21:26:39 -0400

    Make `fromParam` return an `Either`

--- src/Peregrine/Request/FromParam.purs
+++ src/Peregrine/Request/FromParam.purs
@@ -1,5 +1,10 @@
-module Peregrine.Request.FromParam where
+module Peregrine.Request.FromParam
+  ( class FromParam
+  , fromParam
+  ) where
 
+import Prelude
+import Data.Either (Either(..), note)
 import Data.Int as Int
 import Data.Maybe (Maybe(..))
 import Data.Number as Number
@@ -7,25 +12,36 @@ import Data.String.NonEmpty (NonEmptyString)
 import Data.String.NonEmpty as NonEmptyString
 
 class FromParam a where
-  fromParam :: String -> Maybe a
+  fromParam :: String -> Either String a
 
 instance fromParamString :: FromParam String where
-  fromParam = Just
+  fromParam = Right
 
 instance fromParamInt :: FromParam Int where
-  fromParam = Int.fromString
+  fromParam param = Int.fromString >>> note param $ param
 
 instance fromParamNumber :: FromParam Number where
-  fromParam = Number.fromString
+  fromParam param = Number.fromString >>> note param $ param
 
 instance fromParamBoolean :: FromParam Boolean where
   fromParam = case _ of
-    "true" -> Just true
-    "false" -> Just false
-    _ -> Nothing
+    "true" -> Right true
+    "false" -> Right false
+    param -> Left param
 
 instance fromParamNonEmptyString :: FromParam NonEmptyString where
-  fromParam = NonEmptyString.fromString
+  fromParam param = NonEmptyString.fromString >>> note param $ param
 
 instance fromParamMaybe :: (FromParam a) => FromParam (Maybe a) where
-  fromParam = fromParam
+  fromParam =
+    fromParam
+      >>> case _ of
+          Right value -> Right $ Just value
+          Left _ -> Right Nothing
+
+instance fromParamEither :: (FromParam a) => FromParam (Either String a) where
+  fromParam =
+    fromParam
+      >>> case _ of
+          Right value -> Right $ Right value
+          Left param -> Right $ Left param
